912c3b9177b013922856f0aba089d440187f1949
akstech0033 <akstech0033@gmail.com>
2025-02-19 03:52:01 +0530

    Chanegs related to authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
+import { Amplify } from 'aws-amplify';
+import outputs from '../../amplify_outputs.json';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './components/search/search.component';
-
+import { AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/ui-angular';
+Amplify.configure(outputs);
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,9 +16,15 @@ import { SearchComponent } from './components/search/search.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    AmplifyAuthenticatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+    
+  constructor(public authenticator: AuthenticatorService) {
+    Amplify.configure(outputs);
+  }
+}
